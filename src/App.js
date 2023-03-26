de3762212992fc534855b12d9a50b89d8e56b729
merry.lee <merry.lee@goorm.io>
2023-03-26 17:06:21 +0900

    초기 문구 세팅

--- src/App.js
+++ src/App.js
@@ -1,5 +1,16 @@
 function App() {
-    return <div>구름톤 참가자 여러분들~ 환영합니다!</div>;
+    return (
+        <h1
+            style={{
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "center",
+                height: "100vh",
+            }}
+        >
+            구름톤 참가자 여러분들~ 환영합니다!
+        </h1>
+    );
 }
 
 export default App;
